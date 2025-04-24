b323298205d7a90076103f8010dc8517bb4898b6
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 09:02:49 -0300

    ex07

---
+++ ex07/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        bool cadastrado = true;
+        bool cupomValido = false;
+        bool jaUsouCupom = false;
+
+        if (cadastrado && cupomValido && !jaUsouCupom)
+        {
+            Console.WriteLine("Promoção válida! Você pode usar o cupom.");
+        }
+        else
+        {
+            Console.WriteLine("Promoção inválida.");
+        }
+    }
+}
