2c0ae491852c2efe0e1de5e7753e027002722e8b
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 08:41:44 -0300

    ex03

---
+++ ex03/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.AccessControl;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite o nome de usuário: ");
+        string usuario = Console.ReadLine();
+
+        if (usuario == "admin" || usuario == "supervisor")
+        {
+            Console.WriteLine("Acesso concedido");
+        }
+        else
+        {
+            Console.WriteLine("Acesso negado");
+        }
+
+    }
+}
