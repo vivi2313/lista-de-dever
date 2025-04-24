c7cb40f950cf86e0e93fddc0f1b9f0fbfc0a51aa
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 08:59:12 -0300

    ex06

---
+++ ex06/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        bool temIngresso = true;
+        bool documentoValido = true;
+
+        if (temIngresso && documentoValido)
+        {
+            Console.WriteLine("Entrada permitida no evento");
+        }
+        else
+        {
+            Console.WriteLine("Entrada negada no evento");
+        }
+    }
+}
