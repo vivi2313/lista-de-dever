0f8ef2b40a70736ace99ad5732fd2cc2b897864c
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 08:54:14 -0300

    ex04

---
+++ ex04/Program.cs
@@ -0,0 +1,18 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int idade = 19;
+        double renda = 2100.0;
+
+        if (idade > 65 || renda < 2000.0)
+        {
+            Console.WriteLine("Está isento de imposto");
+        }
+        else
+        {
+            Console.WriteLine("Deve pagar imposto");
+        }
+    }
+}
+
