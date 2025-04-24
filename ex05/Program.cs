12cee41bf09ce5677e3d18b2c959e321d11b91bf
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 08:57:00 -0300

    ex05

---
+++ ex05/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        bool ehClienteVip = false;
+        int compras = 2;
+
+        if (!ehClienteVip && compras > 10)
+        {
+            Console.WriteLine("Ganha desconto especial");
+        }
+        else
+        {
+            Console.WriteLine("Não ganha desconto especial");
+        }
+    }
+}
