4634065ce6b21757b32dbe6f288faa05d5b6a218
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 09:08:44 -0300

    ex09

---
+++ ex09/Program.cs
@@ -0,0 +1,16 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int hora = 19;
+
+        if (hora >= 6 && hora <= 22)
+        {
+            Console.WriteLine("Acesso permitido");
+        }
+        else
+        {
+            Console.WriteLine("Fora do horário");
+        }
+    }
+}
