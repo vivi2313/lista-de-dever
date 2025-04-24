2f1690d27d8f4766c7ae94fb9fdd8258e9ec7ed7
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 08:30:41 -0300

    dever01

---
+++ ex01/Program.cs
@@ -0,0 +1,14 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int numero = 25;
+
+        if (numero <= 10 || numero >= 20)
+        {
+            Console.WriteLine(numero);
+        }
+
+    }
+
+}
