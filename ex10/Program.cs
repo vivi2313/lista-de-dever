17037ea1be06b2ba810cb5b2b8c86e4a38ae2824
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 09:12:52 -0300

    dever10

---
+++ ex10/Program.cs
@@ -0,0 +1,17 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int idade = 55;
+        bool comportamentoSuspeito = false;
+
+        if (idade < 18 || comportamentoSuspeito)
+        {
+            Console.WriteLine("Monitorar");
+        }
+        else
+        {
+            Console.WriteLine("Pessoa não suspeita");
+        }
+    }
+}
