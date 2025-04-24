158664420614a1b5fd6143eb37b6b70a70f8fc8a
iguinho-ux <igormachadosr@gmail.com>
2025-04-24 08:32:24 -0300

    ex02

---
+++ ex02/Program.cs
@@ -0,0 +1,19 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        int numero = 20;
+
+        Console.WriteLine("Você tem CNH?: ");
+        string informacao = Console.ReadLine();
+
+        if (numero >= 18 && informacao == "sim")
+        {
+            Console.WriteLine("Pode dirigir");
+        }
+        else
+        {
+            Console.WriteLine("Não pode dirigir");
+        }
+    }
+}
